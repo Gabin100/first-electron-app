1d2d87fcf1a04c9d97eefd6eb8b6675c6470fb43
Jean Gabin Ishimwe <ij.gabin123@gmail.com>
2025-11-30 11:17:46 +0200

    adding constructor

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
-app.whenReady().then(() => {
+app
+  .whenReady()
+  .then(() => {
     console.log('App is ready');
-    const win = BrowserWindow();
-});
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
